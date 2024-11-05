e99f8bcaf906e9fe50d9eda0c1fce42e1946a243
VoidS2 <107724820+VoidS12344@users.noreply.github.com>
2024-11-05 07:51:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Programação',
+    'O que é Lua?',
+    'Lua é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Turquia?',
+    'A capital da Turquia é Istambul'
+)
+
+criaCartao(
+    'Games',
+    'Qual o melhor jogador de Valorant do mundo',
+    'o jogador Aspas que atua na Leviatan'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Verde em Inglês?',
+    'Verde em ingles é Green'
+)
+
+criaCartao(
+    'Química',
+    'O que é uma reação de neutralização?',
+    'É a reação entre um ácido e uma base, que resulta na formação de sal e água.'
+)
+
+criaCartao(
+    'Física',
+    'O que é a Lei de Newton da Gravitação Universal?',
+    'Todo corpo atrai outro com uma força diretamente proporcional ao produto das massas e inversamente proporcional ao quadrado da distância entre eles.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é fotossíntese?',
+    'É o processo pelo qual as plantas, algas e algumas bactérias convertem a luz solar em energia química, produzindo glicose e liberando oxigênio.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é a teoria da deriva continental?',
+    'É a teoria que afirma que os continentes eram um único supercontinente, chamado Pangeia, e que eles se deslocaram ao longo do tempo para suas posições atuais.'
+)
+
+criaCartao(
+    'História',
+    'O que foi a Revolução Francesa?',
+    'Foi um movimento social e político que ocorreu na França entre 1789 e 1799, resultando no fim da monarquia e no início da República Francesa.'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu "Dom Quixote"?',
+    'A obra foi escrita por Miguel de Cervantes, sendo considerada uma das maiores da literatura mundial.'
+)
+
+criaCartao(
+    'Filosofia',
+    'O que é o "Cogito, ergo sum" de René Descartes?',
+    'Significa "Penso, logo existo", uma ideia central do filósofo que afirma que a própria dúvida é prova da existência do indivíduo.'
+)
+
+criaCartao(
+    'Artes',
+    'O que caracteriza o estilo impressionista na pintura?',
+    'O impressionismo se caracteriza pela ênfase em capturar momentos fugazes da luz e da cor, com pinceladas rápidas e cores vibrantes.'
+)
